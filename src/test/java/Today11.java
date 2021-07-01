aa38602b77ea991fb42195f122ec24ca132116ac
Bakyt Kainazarov <bakyt.kainazar@gmail.com>
2021-06-30 20:33:23 -0500

    4

--- src/test/java/Today11.java
+++ src/test/java/Today11.java
@@ -1,5 +1,8 @@
 public class Today11 {
     public static void main(String[] args) {
         System.out.println("333333");
+
+        System.out.println("4444");
+        System.out.println("55555");
     }
 }
