2cfcfc5113be3b4fce74ea2a846def14c647483c
Bakyt Kainazarov <bakyt.kainazar@gmail.com>
2021-06-30 19:31:45 -0500

    third commit

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -6,5 +6,8 @@ public class Hello {
         System.out.println("=======");
         System.out.println("=======");
         System.out.println("=======");
+        System.out.println("=======");
+        System.out.println("=======");
+        System.out.println("=======");
     }
 }
