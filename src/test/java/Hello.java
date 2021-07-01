892b9d6fbcc573e4279f82b6bd8255ab8e73f126
Bakyt Kainazarov <bakyt.kainazar@gmail.com>
2021-06-30 20:16:54 -0500

    Userstory2

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("HelloMy Friend");
+        System.out.println("Hello My Friend");
     }
 }
---
+++ src/test/java/Usstory2.java
@@ -0,0 +1,3 @@
+public class Usstory2 {
+
+}
